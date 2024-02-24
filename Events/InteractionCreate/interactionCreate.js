e7f4103c23a1592de5fcfa8e58ea164039a0b480
lz20r <zhiyaonaiara@gmail.com>
2024-02-24 20:04:37 +0100

    Add createTicket import and fix indentation in interactionCreate.js

--- Events/InteractionCreate/interactionCreate.js
+++ Events/InteractionCreate/interactionCreate.js
@@ -1,16 +1,19 @@
 const momo = require("../../core/client");
+const createTicket = require("../../Slashcmd/tickets/createTicket");
+const { ModalBuilder, ModalSubmitInteraction, ChatInputCommandInteraction, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
 momo.on("interactionCreate", async(interaction) => {
-	
-	if(interaction.channel.type === 'dm') return;
-try{
-
-  if(!interaction.isCommand()) return;
-    const slashcmd = momo.slashcommands.get(interaction.commandName);
     
-	if(!slashcmd) return;
-    await slashcmd.run(momo, interaction);
-  } catch(e) {
-    console.error(e);
-  }
-
-}); 
+    if(interaction.channel.type === 'dm') return;
+    try{
+        
+        if(!interaction.isCommand()) return;
+        const slashcmd = momo.slashcommands.get(interaction.commandName);
+        
+        if(!slashcmd) return;
+        await slashcmd.run(momo, interaction);
+    } catch(e) {
+        console.error(e);
+    }   
+});
+git config --global user.email "zhiyaonaiara@gmail.com"
+  git config --global user.name "lz20r"
